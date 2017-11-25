cf7ab61d14642e3fc87a59691be715f87e30a2ce
khoirlanwr <khoirulanwar.x22@gmail.com>
2017-11-25 16:41:28 +0700

    Update scripy.py

--- scripy.py
+++ scripy.py
@@ -1,6 +1,6 @@
 # Program to convert decimal to biner
 
-1st_count = 0
+st_count = 0
 biner = 0
 base = 1
 bil = input("Decimal: ")
@@ -8,7 +8,7 @@ bil = input("Decimal: ")
 while bil > 0:
     remainder = bil % 2
     if (remainder):
-        1st_count = 1st_count + 1
+        st_count = st_count + 1
     biner = biner + remainder * base
     bil = bil /2
     base = base * 10
